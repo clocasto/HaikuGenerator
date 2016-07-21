039fdac93f5550573e75cb3fe6b28072e8f9d0fa
Chris LoCasto <locasto1@gmail.com>
2016-07-20 23:25:55 -0500

    improved the generator and added comments

--- haiku.js
+++ haiku.js
@@ -1,9 +1,11 @@
+//This wrapper function allows the program to store "global" variables. 'haikuGenerator' is accessible as an export.
 function haikuGenerator(structure) {
 
 	//This corrects the initial structure to be an array in the event that ini_structure
 	//is a string (i.e. when using process.argv to define poem structure). Still requires 
 	//numbers to be comma-delimited.
-	if (!Array.isArray(structure)) structure = structure.replace(/\b[^0-9]+\b/g,",").split(",").map(function(str) {return Number(str);});
+	if (!Array.isArray(structure)) structure = structure.replace(/\b[^0-9]+\b/g,",")
+		.split(",").map(function(str) {return Number(str);});
 
 	//Defining "global" variables - book text, dictionary text, etc.
 	//These variables are not truly global but are local to the wrapper function, 'haikuGenerator'
@@ -13,7 +15,8 @@ function haikuGenerator(structure) {
 		markov_library = markovDict(),
 		poem_line = "";
 		return_poem = "",
-		first_word = "";
+		first_word = wordGrabber(),
+		common_words = "the he she it him her mr ms of and the in from to up down when".toUpperCase();
 
 	//Creates a dictionary object with values of syllables
 	function parseCmuDict() {
@@ -31,6 +34,16 @@ function haikuGenerator(structure) {
 		return count;
 	}
 
+
+	//This function creates the 'markov_library' object. It takes an array of all the words in 'book'
+	//(which is generated from the novels) and adds each word to the 'markov_library' if it is also in the 
+	//CMU Dictionary: 'library'. Furthermore, if the 'next_word' (the word following the 'current_word') is in
+	//the 'library' object, it gets added into the object value of the 'current_word' property of 'markov_library'.
+
+	//The structure of 'markov_library' is an object with a property for each word which appears in both the novels and the 
+	//CMU dictionary. The values are 'WordChain()' objects which contain properties of words ('next_word') which appear 
+	//after the 'markov_library' property words ('current_word'). The values for the 'next_word' properties are the number of times
+	//'next_word' appears after 'current_word'.
 	function markovDict() {
 		var book = formatBookFile(), return_obj = {};
 		
@@ -38,28 +51,26 @@ function haikuGenerator(structure) {
 			var current_word = book[i],
 			next_word = book[i + 1];
 
-			if (library.hasOwnProperty(current_word)) {
+			if (library.hasOwnProperty(current_word) && library.hasOwnProperty(next_word)) {
 				if (!return_obj.hasOwnProperty(current_word)) return_obj[current_word] = new WordChain();
-				if (library.hasOwnProperty(next_word)) {
-					if (!return_obj[current_word].hasOwnProperty(next_word)) {
-						return_obj[current_word][next_word] = 1;
-					} else return_obj[current_word][next_word]++;
-				}
+				if (!return_obj[current_word].hasOwnProperty(next_word)) {
+					return_obj[current_word][next_word] = 1;
+				} else return_obj[current_word][next_word]++;
 			}
 		}
 
 		function formatBookFile() {
-			var return_array = [],match,word_reg = /([A-Za-z]+)[\.\?\! ]/g;
+			var return_array = [],match,word_reg = /\b([\w]+)\b/g;
 
 			function addBook(book_text) {
 				while (match = word_reg.exec(book_text)) {
-				if(!(/\b[^ai]\b/.test(match[1])))
+				if(!(/\b[^sai]\b/.test(match[1])))
 					return_array.push(match[1].toUpperCase());
 				}
 			}
 
 			//Markov chain text "predicting" works best with large amounts of language data. As the 
-			//number of books increases, the phrases get more realistic. With only 'A Tale of Two Cities'
+			//number of books increases, the phrases should get more realistic. With only 'A Tale of Two Cities'
 			//most of the poem was just prepositions. Now, although non-sensical, the poem is closer to actual English
 			//speech patterns.
 
@@ -74,25 +85,27 @@ function haikuGenerator(structure) {
 			//Adds 'Metamorphosis' to the book array input for 'markov_library'
 			var meta_text = fs.readFileSync('./pg5200.txt').toString();
 			addBook(meta_text);
-			//Adds 'Huckleberry Finn' to the book array input for 'markov_library'
-			var huck_text = fs.readFileSync('./pg76.txt').toString();
-			addBook(huck_text);
 			//Adds 'Dracula' to the book array input for 'markov_library'
 			var drac_text = fs.readFileSync('./pg345.txt').toString();
 			addBook(drac_text);
 			//Adds 'Alice in Wonderland' to the book array input for 'markov_library'
 			var alice_text = fs.readFileSync('./pg11.txt').toString();
-			addBook(drac_text);
-			//Adds 'Ulysses' to the book array input for 'markov_library'
-			var ulysses_text = fs.readFileSync('./pg11.txt').toString();
-			addBook(ulysses_text);
+			addBook(alice_text);
 			//Adds 'Peter Pan' to the book array input for 'markov_library'
-			var peter_text = fs.readFileSync('./pg11.txt').toString();
-			addBook(peter_text);
+			var peter_text = fs.readFileSync('./pg16.txt').toString();
+			addBook(peter_text); 
 			//Adds 'The Jungle Book' to the book array input for 'markov_library'
 			var jungle_text = fs.readFileSync('./pg236.txt').toString();
 			addBook(jungle_text);
-
+			//Adds 'The Picture of Dorian Gray' to the book array input for 'markov_library'
+			var dorian_text = fs.readFileSync('./pg174.txt').toString();
+			addBook(dorian_text);
+			//Adds 'Moby Dick' to the book array input for 'markov_library'
+			var moby_text = fs.readFileSync('./pg2701.txt').toString();
+			addBook(moby_text);
+			//Adds 'Sherlock Holmes' to the book array input for 'markov_library'
+			var sherlock_text = fs.readFileSync('./pg1661.txt').toString();
+			addBook(sherlock_text); 
 
 			return return_array;
 		} 
@@ -101,10 +114,18 @@ function haikuGenerator(structure) {
 
 	}
 
+	//Constructor for the object values in the 'markov_library'.
+	
+	//It includes a reference to the 'library' which contains the syllable count for 
+	//all word properties given in the 'library' object.
 	function WordChain() {
 		this.cmuLibrary = library;
 	}
 
+	//This function randomly selects the next word from the 'markov_library' property values, which are
+	//'WordChain()' objects (i.e. 'next_word').
+	
+	//If the selected word does not meet the criteria in 'testWord()' it is repicked.
 	WordChain.prototype.selectRND = function() {
 		var chain_array = [], return_word = "";
 		for (var elt in this) {
@@ -119,16 +140,24 @@ function haikuGenerator(structure) {
 		while (testWord(return_word)) {
 			return_word = chain_array[Math.floor(Math.random() * chain_array.length)];
 		}
+
+		//Prepositions and other common words appear a bit too often. The block below will repick a common word 60% of the time.
+		//While not an organic solution, it improves the quality of the poems in spite of the constraints of the haiku structure.
+		if (new RegExp(return_word).test(common_words) && (Math.floor(Math.random() * 5) + 1) > 2 ) {
+			return_word	= this.selectRND();
+		}
+
 		return return_word;
 	}
 
+	//This function tests that a 'markov_library' property has a word in its 'WordChain()' object which
+	//is both in the 'library' object and meets the syllable requirement of the current poem line.
 	WordChain.prototype.hasWord = function(syll_count,test_str) {
 		for (var elt in this) {
 			if (this.hasOwnProperty(elt) && this.cmuLibrary.hasOwnProperty(elt)) {
 				if (this.wordLength(elt) <= syll_count && !(testWord(elt))) return true;
 			}
 		}
-
 		return false;
 	}
 
@@ -136,6 +165,8 @@ function haikuGenerator(structure) {
 		return this.cmuLibrary[word];
 	}
 
+	//This function tests if a 'word' is in the current poem or current line. All duplicate words
+	//were removed from the poem to produce higher quality poems/words.
 	function testWord(word) {
 		if (word === "") return false;
 		if (new RegExp(word).test(return_poem) || new RegExp(word).test(poem_line)) return true;
@@ -147,7 +178,13 @@ function haikuGenerator(structure) {
 		return keys_array[Math.floor(Math.random() * keys_array.length)];
 	}
 
-	function markovLineMaker(line_length,first_word) {
+	//This function creates the poem lines using Markov Chains. It will start with the 'first_word', which is random for the start
+	//of the poem and equal to the last word in the previous line otherwise. From there, it will look up the 'current_word'
+	//in the 'markov_library' and select a random word. If the 'line_length' (syllable requirement) of the line would be
+	//exceeded by the proposed 'next_word' (stored in 'temp_word' and 'word_length'), then the temp_word is repicked. The 
+	//function will never search for a 'next_word' unless it confirms a valid word exists in the 
+	//'markov_library[current_word]' ('WordChains()') object through the 'hasWord()' method.
+	function markovLineMaker(line_length) {
 		var	current_word = first_word,temp_word,word_length = library[first_word];
 		poem_line = "";
 
@@ -165,7 +202,7 @@ function haikuGenerator(structure) {
 					current_word = temp_word;
 					markov_state = true;
 				}
-
+				
 				poem_line += current_word + " ";
 				line_length -= word_length;
 
@@ -173,21 +210,19 @@ function haikuGenerator(structure) {
 
 		}
 
-		first_word = poem_line.slice(0,-1).search(/\b[\W]+$/);
+		first_word = /\b([\w]+)$/.exec(poem_line.slice(0,-1))[1];
 		return poem_line.slice(0,-1);
 	}  
 
+	//This function assembles the lines according to the specified structure.
 	function createHaiku(structure) {
-	
 		for (var i = 0; i < structure.length; i++) {
-	
-			return_poem += markovLineMaker(structure[i],
-				first_word === "" ? wordGrabber() : first_word) + '\n\n';
+			return_poem += markovLineMaker(structure[i]) + '\n\n';
 		}
-
 		return return_poem.slice(0,-2);
 	} 
 
+	//Wrapper return statement
 	return createHaiku(structure);
 
 }
--- haiku_generator.js
+++ haiku_generator.js
@@ -1,9 +1,15 @@
 var haiku = require('./haiku');
 
-
 //User can specify poem structure as a second argument in any punctuation-delimited manner. Spaces/new lines will not work.
 var poem_style = process.argv[2] == undefined ? [5,7,5] : process.argv[2];
 
+//The time it takes to write the poem will be recorded. Given how much data is processed, it is important to be aware of.
 var time_zero = Date.now();
 console.log('\n' + haiku.haikuGenerator(poem_style));
 console.log("\nThis poem took me " + (Date.now() - time_zero) + "ms to write.\n")
+
+
+//NOTE: This program has a bug in it which remains unsolved. Once every ~25 poems 'markovLineMaker()' will try to access a word in the 'markov_library'
+//		which does not exist. Given that no words are entered into the 'markov_library' unless they are in the CMU dictionary, have at 
+//		least one valid 'next_word', and the program never asks for a word which isn't a property of the 'markov_library',
+//		I haven't be able to find it or reproduce it.
